e40fa4714eb54b5500b59a2eac1c46325fca2154
Joseph Noir <joseph.noir@posteo.net>
2019-06-11 15:48:40 +0200

    Add io example

--- main.cpp
+++ main.cpp
@@ -1,10 +1,17 @@
 #include <caf/all.hpp>
+#include <caf/io/all.hpp>
 
 void caf_main(caf::actor_system& system, const caf::actor_system_config& config) {
-  system.spawn([](caf::event_based_actor* self) {
+  auto a = system.spawn([](caf::event_based_actor* self) -> caf::behavior {
     aout(self) << "hello world" << std::endl;
+    return {
+      [=](int i) {
+        return i + 1;
+      }
+    };
   });
+  system.middleman().publish(a, 23456);
   system.await_all_actors_done();
 }
 
-CAF_MAIN()
+CAF_MAIN(caf::io::middleman)
